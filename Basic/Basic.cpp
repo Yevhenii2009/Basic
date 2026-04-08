052a51d45a7b9a926f7f9dbaf525336137f4fa0c
Yevhenii2009 <kyaacdc2@gmail.com>
2026-04-08 21:51:36 +0100

    For

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -1,6 +1,3 @@
-// Basic.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
@@ -11,21 +8,7 @@ int main()
 	std::cin >> num;
 	std::cout << "write first second" << std::endl;
 	std::cin >> num2;
-	while (num <= num2) {
-		std::cout << num << std::endl;
-		num++;
-	}
-
+	for (int i = num; i <= num2; ++i)
+		std::cout << i << std::endl;
 	return 0; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
