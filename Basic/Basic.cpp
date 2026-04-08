7a519ee43f3d848f52e0506a9bd4c130366a36f9
Yevhenii2009 <kyaacdc2@gmail.com>
2026-04-08 19:35:21 +0100

    sum

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -5,7 +5,10 @@
 
 int main()
 {
-	return -1;
+	std::cout << "Enter two numbers: " << std::endl;
+	int v1 = 0; int v2 = 0;
+	std::cout << "The	sum	of	" << v1 << "	and	" << v2
+		<< "	is	" << v1 + v2 << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
